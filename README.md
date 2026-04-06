dc2251b5e564ea03fb252404d1c4da702e683ca1
BadLuckZ <thanagorn8802@gmail.com>
2026-04-06 17:39:35 +0700

    chore: runner

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Study Spring Boot
 
 Resource: [Spring Boot](https://www.jetbrains.com/help/idea/spring-support.html)
+- Database Connection
+  - https://www.jetbrains.com/help/idea/relational-databases.html#first-steps
+  - https://www.jetbrains.com/help/idea/data-sources-and-drivers-dialog.html#overview
--- src/main/java/com/example/springboot_learn/SpringbootLearnApplication.java
+++ src/main/java/com/example/springboot_learn/SpringbootLearnApplication.java
@@ -1,7 +1,9 @@
 package com.example.springboot_learn;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringbootLearnApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootLearnApplication.class, args);
+    }
+
+    @Bean
+    // Log ที่จะแสดงทันทีหลังจากที่ run server สำเร็จ
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner->{
+            System.out.println("Running this server at localhost:8080");
+        };
     }
 }
